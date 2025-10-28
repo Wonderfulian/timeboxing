a12c70b2f29ed5ee2a82f20feef1a77b41613f6f
Wonderfulian <creativeyoungcho@gmail.com>
2025-10-28 14:24:37 +0900

    Add files via upload

--- service-worker.js.js
+++ service-worker.js.js
@@ -1,25 +1,42 @@
-const CACHE_NAME='timeboxing-v6';
-const FILES_TO_CACHE=[
+const CACHE_NAME = 'timeboxing-v7';
+const FILES_TO_CACHE = [
   './',
   './index.html',
   './manifest.json',
-  './icons/icon-192.png',
-  './icons/icon-512.png'
+  './icon-192.png',
+  './icon-512.png',
+  'https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;600;700&display=swap',
+  'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js'
 ];
 
-self.addEventListener('install',e=>{
-  e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(FILES_TO_CACHE)));
-});
-self.addEventListener('fetch',e=>{
-  e.respondWith(
-    caches.match(e.request).then(r=>r||fetch(e.request))
-  );
-});
-self.addEventListener('activate',e=>{
+self.addEventListener('install', e => {
+  console.log('[Service Worker] Install');
   e.waitUntil(
-    caches.keys().then(keys=>Promise.all(
-      keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k))
-    ))
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('[Service Worker] Caching files');
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
   );
 });
 
+self.addEventListener('activate', e => {
+  console.log('[Service Worker] Activate');
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      );
+    })
+  );
+  self.clients.claim();
+});
