a05db9a85d77a02938705e49a1f265f58e8ac034
Wonderfulian <creativeyoungcho@gmail.com>
2025-10-28 14:36:40 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,63 @@
+const CACHE_NAME = 'timeboxing-v8';
+const urlsToCache = [
+  '/timeboxing/',
+  '/timeboxing/index.html',
+  '/timeboxing/manifest.json',
+  '/timeboxing/icon-192.png',
+  '/timeboxing/icon-512.png'
+];
+
+// 설치 이벤트
+self.addEventListener('install', event => {
+  console.log('[Service Worker] Install');
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('[Service Worker] Caching files');
+        return cache.addAll(urlsToCache);
+      })
+      .then(() => self.skipWaiting())
+  );
+});
+
+// 활성화 이벤트
+self.addEventListener('activate', event => {
+  console.log('[Service Worker] Activate');
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('[Service Worker] 오래된 캐시 삭제:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+// 페치 이벤트
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // 캐시에 있으면 반환, 없으면 네트워크 요청
+        if (response) {
+          return response;
+        }
+        return fetch(event.request).then(response => {
+          // 유효한 응답인지 확인
+          if (!response || response.status !== 200 || response.type !== 'basic') {
+            return response;
+          }
+          // 응답 복사본을 캐시에 저장
+          const responseToCache = response.clone();
+          caches.open(CACHE_NAME).then(cache => {
+            cache.put(event.request, responseToCache);
+          });
+          return response;
+        });
+      })
+  );
+});
